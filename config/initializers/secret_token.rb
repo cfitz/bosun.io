de2c5153a524a855784185b271e95b456846c0a1
fitz <chrisfitzpat@gmail.com>
2013-01-29 11:19:25 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BosunIo::Application.config.secret_token = 'b67e62c4264189fa27334a79dc9a148c3f847d189baf820fd3db8aed66800bc3b85d87e1b65b080b2e126f85407968b18d3488f962cad02e097b865027f2cfb0'
